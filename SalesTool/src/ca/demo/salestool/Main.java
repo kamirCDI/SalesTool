1a74e57289e89837e55499208d7e7341cc7f3d17
kamir <Kamir.oudj@gmail.com>
2015-04-07 22:48:33 +0200

    add a second change

--- SalesTool/src/ca/demo/salestool/Main.java
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -8,6 +8,7 @@ public class Main {
 		System.out.println("HELLO HAPPY SALES PEOPLE");
         System.out.println("THIS APP SHOWS SALEs DATA");
         System.out.println("test1");
+        System.out.println("test2");
 	}
 
 }
