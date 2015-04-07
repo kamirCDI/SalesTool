63e128fc065bd5038010c2f8b695a9d772f006b4
kamir <Kamir.oudj@gmail.com>
2015-04-07 22:06:16 +0200

    Initial checkin in SalesTool

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,12 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("HELLO HAPPY SALES PEOPLE");
+        System.out.println("THIS APP SHOWS SALEs DATA");
+	}
+
+}
