3eab611d5143522713183f5fc923a47d656202ab
kamir <Kamir.oudj@gmail.com>
2015-04-07 22:53:05 +0200

    add a third change

--- SalesTool/src/ca/demo/salestool/Main.java
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("THIS APP SHOWS SALEs DATA");
         System.out.println("test1");
         System.out.println("test2");
+        System.out.println("test3");
 	}
 
 }
