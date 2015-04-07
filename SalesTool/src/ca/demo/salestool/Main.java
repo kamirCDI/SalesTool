9c418ad6317750f0276355f9209ffac5c3b1eaec
kamir <Kamir.oudj@gmail.com>
2015-04-07 23:13:12 +0200

    a essai on fixcap

--- SalesTool/src/ca/demo/salestool/Main.java
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -10,6 +10,7 @@ public class Main {
         System.out.println("test1");
         System.out.println("test2");
         System.out.println("test3");
+        System.out.println("testfixcap");
 	}
 
 }
