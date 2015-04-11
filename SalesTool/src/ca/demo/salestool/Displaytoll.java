b67b1d40b64d5387af7fdff4a04a0c6e03fb9160
kamir <Kamir.oudj@gmail.com>
2015-04-11 10:08:03 +0200

    n

--- SalesTool/src/ca/demo/salestool/Displaytoll.java
+++ SalesTool/src/ca/demo/salestool/Displaytoll.java
@@ -3,13 +3,18 @@ package ca.demo.salestool;
 public class Displaytoll {
 /****************************************************/
 	private String  nom;
-	String prenom;
+	private String prenom;
 	private String adresse;
+	private String preno;
+	private String pronom;
 	private String local;	
 	private int d;
 	private String preno;
 	private String pronom;
 		
+
+	private String local;	
+	private String local;	
 	
 	
 }
