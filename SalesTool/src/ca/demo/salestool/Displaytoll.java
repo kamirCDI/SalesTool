bde6b9a18ee129eb8922afb5d9e5974974d14264
kamir <Kamir.oudj@gmail.com>
2015-04-11 10:14:06 +0200

    Revert "n"
    This reverts commit b67b1d40b64d5387af7fdff4a04a0c6e03fb9160.

--- SalesTool/src/ca/demo/salestool/Displaytoll.java
+++ SalesTool/src/ca/demo/salestool/Displaytoll.java
@@ -3,18 +3,13 @@ package ca.demo.salestool;
 public class Displaytoll {
 /****************************************************/
 	private String  nom;
-	private String prenom;
+	String prenom;
 	private String adresse;
-	private String preno;
-	private String pronom;
 	private String local;	
 	private int d;
 	private String preno;
 	private String pronom;
 		
-
-	private String local;	
-	private String local;	
 	
 	
 }
